5e2031d166574d421f2f9c2c399082ae1a41884e
AbijuruT <abijuruyves@gmail.com>
2024-07-12 12:49:49 +0200

    Step to resolve the callback hell

--- async.js
+++ async.js
@@ -7,35 +7,35 @@ let stocks = {
 
 
 
-let order = (fruitName, callProduction) => {
-  setTimeout(() => {
-    console.log(`${stocks.fruits[fruitName]} was selected!`)
-    callProduction();
-  }, 2000);
-};
-let production = () => {
-  setTimeout(() => {
-    console.log("Production has started");
-    setTimeout(() => {
-      console.log("The fruit has been chopped!");
-      setTimeout(() => {
-        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added!`);
-        setTimeout(() => {
-          console.log("Machine was started :)");
-          setTimeout(() => {
-            console.log(`Ice cream was placed on the ${stocks.holder[0]}`);
-            setTimeout(() => {
-              console.log(`${stocks.topping[0]} was added`);
-              setTimeout(() => {
-                console.log('Ice cream is ready')
-              }, 2000)
-            }, 3000)
-          }, 2000);
-        }, 1000);
-      }, 1000)
-    }, 2000)
+// let order = (fruitName, callProduction) => {
+//   setTimeout(() => {
+//     console.log(`${stocks.fruits[fruitName]} was selected!`)
+//     callProduction();
+//   }, 2000);
+// };
+// let production = () => {
+//   setTimeout(() => {
+//     console.log("Production has started");
+//     setTimeout(() => {
+//       console.log("The fruit has been chopped!");
+//       setTimeout(() => {
+//         console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added!`);
+//         setTimeout(() => {
+//           console.log("Machine was started :)");
+//           setTimeout(() => {
+//             console.log(`Ice cream was placed on the ${stocks.holder[0]}`);
+//             setTimeout(() => {
+//               console.log(`${stocks.topping[0]} was added`);
+//               setTimeout(() => {
+//                 console.log('Ice cream is ready')
+//               }, 2000)
+//             }, 3000)
+//           }, 2000);
+//         }, 1000);
+//       }, 1000)
+//     }, 2000)
 
-  }, 0)
-};
+//   }, 0)
+// };
 
-order(0, production)
+// order(0, production)
