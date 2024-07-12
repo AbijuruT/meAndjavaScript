d2af983baf2b2ca10054a6ef0617f0f1e16b74e3
AbijuruT <abijuruyves@gmail.com>
2024-07-12 12:05:50 +0200

    Call back hell

--- async.js
+++ async.js
@@ -16,6 +16,12 @@ let order = (fruitName, callProduction) => {
 let production = () => {
   setTimeout(() => {
     console.log("Production has started");
+    setTimeout(() => {
+      console.log("The fruit has been chopped!");
+      setTimeout(() => {
+        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added!`);
+      }, 1000)
+    }, 2000)
 
   }, 0)
 };
